0711aac7e23627b47578c1967f35572894b8b06d
Luke Bouch <lukebouch2014@gmail.com>
2021-10-07 22:17:14 -0400

    ➕ Add title

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 return [
     'production' => false,
     'baseUrl' => '',
-    'title' => 'Jigsaw',
-    'description' => 'Website description.',
+    'title' => 'Bouch Contracting',
+    'description' => '',
     'collections' => [],
 ];
