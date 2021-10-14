59d1cffd10d35689c1d1ff2e5d5dcf0be8bf0da5
Luke Bouch <lukebouch2014@gmail.com>
2021-10-14 16:20:12 -0400

    ➕ Add new hero headline

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,9 +3,13 @@
 @section('body')
 <div class="h-screen p-8 bg-cover"
     style="background-image: linear-gradient(rgba(227, 114, 38, 0.75),rgba(227, 114, 38, 0.75)), url('/assets/images/hero.jpg') ;">
-    <div class="h-full max-w-3xl mx-auto flex flex-col justify-center items-center">
-        <img src="/assets/images/logo-white.png" alt="">
-        <h1 class="text-5xl text-center text-white font-bold">Coming Soon</h1>
+    <div class="h-full max-w-5xl mx-auto flex flex-col justify-center items-center">
+
+        <h1 class="text-5xl text-center text-white font-bold">Make Your Home
+            <div class="text-xl">
+                Modern + Organized + Functional
+            </div>
+        </h1>
     </div>
 </div>
 @endsection
