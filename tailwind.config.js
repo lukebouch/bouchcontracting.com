4c2c9719874dad15f18543c39a24f9664b9b465d
Luke Bouch <lukebouch2014@gmail.com>
2021-10-14 16:19:55 -0400

    👍 Improve font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
     purge: ["source/**/*.blade.php", "source/**/*.md", "source/**/*.html"],
     theme: {
-        extend: {},
+        extend: {
+            fontSize: {
+                lg: "1.333rem",
+                xl: "1.777rem",
+                "2xl": "2.369rem",
+                "3xl": "3.157rem",
+                "4xl": "4.209rem",
+                "5xl": "5.610rem",
+            },
+        },
     },
     variants: {
         extend: {},
