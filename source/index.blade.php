6422dff16138ca2aba92fd5c3cb2c395e529d14d
Luke Bouch <lukebouch2014@gmail.com>
2021-10-31 22:18:51 -0400

    Revert "👍 Update hero title"
    This reverts commit e2cd1eb2731ec8ce51d0af860054f2877cdca1be.

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,11 +3,11 @@
 @section('body')
 <div class="h-screen p-8 bg-cover"
     style="background-image: linear-gradient(rgba(227, 114, 38, 0.75),rgba(227, 114, 38, 0.75)), url('/assets/images/hero.jpg') ;">
-    <div class="h-full mx-auto flex flex-col justify-center items-center">
+    <div class="h-full max-w-5xl mx-auto flex flex-col justify-center items-center">
 
-        <h1 class="text-5xl text-center text-white font-bold">Transform Your Home
+        <h1 class="text-5xl text-center text-white font-bold">Make Your Home
             <div class="text-xl">
-                We are Friendly + Trustworthy + Profesional.
+                Modern + Organized + Functional
             </div>
         </h1>
     </div>
