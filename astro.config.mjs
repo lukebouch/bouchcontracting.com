389f194c39d32ec3acee7f7e0713f16d1fa792b6
Luke Bouch <luke@lukebouch.com>
2023-07-04 11:00:18 -0400

    Add Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), vue()]
 });
