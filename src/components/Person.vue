f3744c4c94ce6934248a59322a9da2a0056447f3
Luke Bouch <luke@lukebouch.com>
2023-07-07 17:19:58 -0400

    Add team page

---
+++ src/components/Person.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img
+            class="w-full rounded-lg shadow-md"
+            :src="person.images.small"
+            :srcset="
+                person.images.medium + ' 2x, ' + person.images.large + ' 3x'
+            "
+        />
+        <h2
+            class="mt-4 text-2xl font-medium text-gray-800"
+            v-text="person.name"
+        ></h2>
+        <p class="text-gray-600" v-text="person.title"></p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["person"],
+};
+</script>
---
+++ src/data/team.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        name: "Tom Bouch",
+        title: "Owner",
+        images: {
+            small: "https://static.bouchcontracting.com/team/tom-bouch-sm.jpg",
+            medium: "https://static.bouchcontracting.com/team/tom-bouch-md.jpg",
+            large: "https://static.bouchcontracting.com/team/tom-bouch-lg.jpg",
+        },
+    },
+    {
+        name: "Carson Bouch",
+        title: "",
+        images: {
+            small: "https://static.bouchcontracting.com/team/carson-bouch-sm.jpg",
+            medium: "https://static.bouchcontracting.com/team/carson-bouch-md.jpg",
+            large: "https://static.bouchcontracting.com/team/carson-bouch-lg.jpg",
+        },
+    },
+    {
+        name: "Carter MacDonald",
+        title: "",
+        images: {
+            small: "https://static.bouchcontracting.com/team/carter-macdonald-sm.jpg",
+            medium: "https://static.bouchcontracting.com/team/carter-macdonald-md.jpg",
+            large: "https://static.bouchcontracting.com/team/carter-macdonald-lg.jpg",
+        },
+    },
+    {
+        name: "Mike Temple",
+        title: "Project Manager",
+        images: {
+            small: "https://static.bouchcontracting.com/team/mike-temple-sm.jpg",
+            medium: "https://static.bouchcontracting.com/team/mike-temple-md.jpg",
+            large: "https://static.bouchcontracting.com/team/mike-temple-lg.jpg",
+        },
+    },
+];
