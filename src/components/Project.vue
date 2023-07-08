cb0fd974d891ef6dc45bd3e8c1e42f85df8889bd
Luke Bouch <luke@lukebouch.com>
2023-07-07 21:46:16 -0400

    Add Our Work page

---
+++ src/components/Project.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-14">
+        <img
+            class="rounded-xl shadow-md"
+            :src="project.images[0].src"
+            :alt="project.images[0].src"
+        />
+        <div>
+            <div class="mb-8">
+                <h2 class="text-4xl font-semibold" v-text="project.title"></h2>
+                <p
+                    class="mt-3"
+                    v-if="project.description"
+                    v-text="project.description"
+                ></p>
+            </div>
+
+            <div class="grid grid-cols-2 gap-10">
+                <img
+                    class="rounded-xl shadow-md"
+                    :src="image.src"
+                    :alt="image.alt"
+                    v-for="image in project.images.slice(1)"
+                    :id="image.src"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["project"],
+};
+</script>
---
+++ src/data/projects.js
@@ -0,0 +1,40 @@
+export default [
+    {
+        title: "Northridge Kitchen Remodel",
+        description:
+            "Nunc varius, lorem ac porttitor luctus, risus dui ornare nibh, eu feugiat ipsum purus et magna.",
+        images: [
+            {
+                src: "https://static.bouchcontracting.com/portfolio/northridge-kitchen-remodel-04.jpg",
+                alt: "",
+            },
+            {
+                src: "https://static.bouchcontracting.com/portfolio/northridge-kitchen-remodel-02.jpg",
+                alt: "",
+            },
+            {
+                src: "https://static.bouchcontracting.com/portfolio/northridge-kitchen-remodel-07.jpg",
+                alt: "",
+            },
+        ],
+    },
+    {
+        title: "Commonwealth Kitchen Remodel",
+        description:
+            "Quisque feugiat feugiat enim, eget viverra magna hendrerit et. Etiam id purus libero. Phasellus sem turpis, faucibus ac tellus a, interdum aliquet mauris.",
+        images: [
+            {
+                src: "https://static.bouchcontracting.com/portfolio/commonwealth-kitchen-remodel-03.jpg",
+                alt: "",
+            },
+            {
+                src: "https://static.bouchcontracting.com/portfolio/commonwealth-kitchen-remodel-04.jpg",
+                alt: "",
+            },
+            {
+                src: "https://static.bouchcontracting.com/portfolio/commonwealth-kitchen-remodel-06.jpg",
+                alt: "",
+            },
+        ],
+    },
+];
