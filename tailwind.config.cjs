fd7d53ce990960d174befda4045ab031878faae6
Luke Bouch <luke@lukebouch.com>
2023-07-08 18:41:01 -0400

    Fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,9 +19,9 @@ module.exports = {
                 },
             },
         },
-        plugins: [
-            require("@tailwindcss/typography"),
-            require("@tailwindcss/forms"),
-        ],
     },
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+    ],
 };
