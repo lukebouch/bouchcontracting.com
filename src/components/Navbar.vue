68c5a52a988b0bed50f2c2dba7fb648ad0404ddf
Luke Bouch <luke@lukebouch.com>
2023-07-07 22:38:58 -0400

    Update tel link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -97,7 +97,7 @@
                         </div>
                         <div class="py-6">
                             <a
-                                href="#"
+                                href="tel:+14349222500"
                                 class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
                                 >(434) 922-2500</a
                             >
