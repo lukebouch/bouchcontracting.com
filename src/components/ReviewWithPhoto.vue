e7cbefe7b418826dd7320a6806ec5c18ac169d42
Luke Bouch <luke@lukebouch.com>
2023-07-09 16:24:12 -0400

    WIP

--- src/components/ReviewWithPhoto.vue
+++ src/components/ReviewWithPhoto.vue
@@ -1,40 +1,34 @@
 <template>
-    <section>
-        <div class="h-screen relative">
-            <div
-                class="absolute z-10 inset-0 h-full w-full flex flex-col justify-center"
-            >
-                <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 p-10">
-                    <div
-                        :class="[
-                            'bg-white p-5',
-                            'shadow-lg',
-                            'max-w-2xl',
-                            'mx-auto',
-                            { 'lg:col-start-2': flip },
-                        ]"
-                    >
-                        <h3 class="text-4xl font-semibold mb-5">{{ title }}</h3>
-                        <figure>
-                            <blockquote class="mb-2 text-lg">
-                                <slot></slot>
-                            </blockquote>
-                            <figcaption>
-                                <span
-                                    class="font-semibold text-lg text-gray-800"
-                                    >{{ name }}</span
-                                >
-                            </figcaption>
-                        </figure>
-                    </div>
+    <div class="h-screen relative">
+        <div
+            class="absolute z-10 inset-0 h-full w-full flex flex-col justify-center"
+        >
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 p-10">
+                <div
+                    :class="[
+                        'bg-white p-5',
+                        'shadow-lg',
+                        'max-w-2xl',
+                        'mx-auto',
+                        { 'lg:col-start-2': flip },
+                    ]"
+                >
+                    <h3 class="text-4xl font-semibold mb-5">{{ title }}</h3>
+                    <figure>
+                        <blockquote class="mb-2 text-lg">
+                            <slot></slot>
+                        </blockquote>
+                        <figcaption>
+                            <span class="font-semibold text-lg text-gray-800">{{
+                                name
+                            }}</span>
+                        </figcaption>
+                    </figure>
                 </div>
             </div>
-            <img
-                class="absolute inset-0 h-full w-full object-cover"
-                :src="img"
-            />
         </div>
-    </section>
+        <img class="absolute inset-0 h-full w-full object-cover" :src="img" />
+    </div>
 </template>
 
 <script>
