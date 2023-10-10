cbba4290cac8d7311babf92cdf434d2bfa05e65e
Luke Bouch <luke@lukebouch.com>
2023-10-09 23:25:39 -0400

    Add margin around sections on homepage

--- src/components/ReviewWithPhoto.vue
+++ src/components/ReviewWithPhoto.vue
@@ -1,33 +1,35 @@
 <template>
-    <div class="h-screen relative">
-        <div
-            class="absolute z-10 inset-0 h-full w-full flex flex-col justify-center"
-        >
-            <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 p-10">
-                <div
-                    :class="[
-                        'bg-white p-5',
-                        'shadow-lg',
-                        'max-w-2xl',
-                        'mx-auto',
-                        { 'lg:col-start-2': flip },
-                    ]"
-                >
-                    <h3 class="text-4xl font-semibold mb-5">{{ title }}</h3>
-                    <figure>
-                        <blockquote class="mb-2 text-lg">
-                            <slot></slot>
-                        </blockquote>
-                        <figcaption>
-                            <span class="font-semibold text-lg text-gray-800">{{
-                                name
-                            }}</span>
-                        </figcaption>
-                    </figure>
+    <div class="h-screen">
+        <div class="h-full relative m-20">
+            <div
+                class="absolute z-10 inset-0 h-full w-full flex flex-col justify-center"
+            >
+                <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 p-10">
+                    <div
+                        :class="[
+                            'bg-white p-5',
+                            'shadow-lg',
+                            'max-w-2xl',
+                            'mx-auto',
+                            { 'lg:col-start-2': flip },
+                        ]"
+                    >
+                        <h3 class="text-4xl font-semibold mb-5">{{ title }}</h3>
+                        <figure>
+                            <blockquote class="mb-2 text-lg">
+                                <slot></slot>
+                            </blockquote>
+                            <figcaption>
+                                <span class="font-semibold text-lg text-gray-800">{{
+                                    name
+                                }}</span>
+                            </figcaption>
+                        </figure>
+                    </div>
                 </div>
             </div>
+            <img class="absolute inset-0 h-full w-full object-cover" :src="img" />
         </div>
-        <img class="absolute inset-0 h-full w-full object-cover" :src="img" />
     </div>
 </template>
 
