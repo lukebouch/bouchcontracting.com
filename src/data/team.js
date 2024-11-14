37884a14ebecfc2bfc793356ad38ab58e4040d04
Luke Bouch <luke@lukebouch.com>
2024-11-14 17:13:43 -0500

    Update team member section

--- src/data/team.js
+++ src/data/team.js
@@ -1,48 +1,47 @@
-import tomBouchImage01 from "../assets/images/people/tom-bouch-01.jpg";
-import carterMacdonaldImage01 from "../assets/images/people/carter-macdonald-01.jpg";
-import mikeTempleImage01 from "../assets/images/people/mike-temple-01.jpg";
-import nathanHollifieldImage01 from "../assets/images/people/nathan-hollifield-01.jpg";
-import wadeHooverImage01 from "../assets/images/people/wade-hoover-01.jpg";
+import tomBouchImage from "../assets/images/people/tom-bouch-02.jpg";
+import carsonBouchImage from "../assets/images/people/carson-bouch-02.jpg";
+import mikeTempleImage from "../assets/images/people/mike-temple-02.jpg";
+import charlieBenzImage from "../assets/images/people/charlie-benz-02.jpg";
+import benBouchImage from "../assets/images/people/benjamin-bouch-02.jpg";
+import jorgeBeltranImage from "../assets/images/people/jorge-beltran-02.jpg";
+import brandonWilliamsImage from "../assets/images/people/brandon-williams-02.jpg";
 
 export default [
     {
         name: "Tom Bouch",
         title: "Owner",
-        image: tomBouchImage01,
+        image: tomBouchImage,
         bio: "He enjoys working with customers and riding motorcycles.",
-    },
-    {
-        name: "Carson Bouch",
-        title: "",
-        image: null,
-    },
-    {
-        name: "Carter MacDonald",
-        title: "",
-        image: carterMacdonaldImage01,
-        bio: "His hobbies are hiking, photography, and writing and recording music."
     },
     {
         name: "Mike Temple",
         title: "Project Manager",
-        image: mikeTempleImage01,
+        image: mikeTempleImage,
         bio: "He enjoys spending time with his family, serving at church, or going off in the woods hunting."
     },
     {
-        name: "Nathan Hollifield",
+        name: "Carson Bouch",
         title: "",
-        image: nathanHollifieldImage01,
-        bio: "He keeps the job site light-hearted and fun with his sense of humor and friendly personality."
-    },
-    {
-        name: "Wade Hoover",
-        title: "",
-        image: wadeHooverImage01,
-        bio: "He has a huge pasion for evangelism and you'll often find him sharing the gospel."
+        image: carsonBouchImage,
     },
     {
         name: "Charlie Benz",
         title: "",
-        image: null,
+        image: charlieBenzImage,
+    },
+    {
+        name: "Jorge Beltran",
+        title: "",
+        image: jorgeBeltranImage,
+    },
+    {
+        name: "Ben Bouch",
+        title: "",
+        image: benBouchImage,
+    },
+    {
+        name: "Brandon Williams",
+        title: "",
+        image: brandonWilliamsImage,
     },
 ];
